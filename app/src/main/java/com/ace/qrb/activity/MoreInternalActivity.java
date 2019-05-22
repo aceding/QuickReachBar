35dfa461be5758b1af98b5fcebb6bb7741f8681b
aceding <149987549@qq.com>
2019-05-22 21:57:14 +0800

    modify the feature.

--- app/src/main/java/com/ace/qrb/activity/MoreInternalActivity.java
+++ app/src/main/java/com/ace/qrb/activity/MoreInternalActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.ace.qrb.QuickReachBar;
 import com.ace.qrb.R;
 
 public class MoreInternalActivity extends FragmentActivity {
@@ -25,7 +26,8 @@ public class MoreInternalActivity extends FragmentActivity {
 
             @Override
             public void onClick(View view) {
-
+                QuickReachBar quickReachBar = QuickReachBar.createQuickReachBar(MoreInternalActivity.this);
+                quickReachBar.show();
             }
         });
 
